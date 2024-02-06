40fae0b3c922d27a6c79b1c35d3cf92c672037b9
connor-lynchh <cowlynch@ucsc.edu>
2024-02-06 04:56:18 -0800

    coding complete

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -1,67 +1,112 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - Experiment 4 Images, Video, and Sound Art
+// Author: Connor Lynch
+// Date: 2/5/2024
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+// Constants - User-serviceable parts
+// In a longer project, I like to put these in a separate file
 
-// Globals
-let myInstance;
-let canvasContainer;
+let cubVid; // Variable to hold the video capture
+let x, y; // Variables for the position of the video feed
+let xspeed, yspeed; // Variables for the speed of the video feed
+let videoColor; // Variable for the current color of the video feed
+let gong; // Variable for the ding sound
+let maxSpeed = 10;  // Maximum speed
+let minSpeed = 3;   // Minimum speed
+let acceleration = 1.1;  // Acceleration rate
+let deceleration = 0.9;  // Deceleration rate
+let speedingUp = true;   // Flag to determine if we are speeding up or slowing down
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
+function preload() {
+  // Load the ding sound here
+  soundFormats('mp3','ogg');
+  // Make sure to have a "ding.mp3" file in your project directory, or change the filename as needed
+  gong = loadSound('js/gong_sound.mp3');
 }
 
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+  createCanvas(2440, 1440); // Set the canvas size
+  cubVid = createCapture(VIDEO); // Start capturing video from the webcam
+  cubVid.hide(); // Hide the actual video element
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  // Initialize position to the center of the canvas
+  x = width / 2;
+  y = height / 2;
+
+  // Initialize speed for both directions
+  xspeed = 3;
+  yspeed = 3;
+
+  // Initialize the video feed color to white
+  videoColor = color(100,1);
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  background(0); // Set the background to black
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  // Draw the video feed as an image tinted with the current color
+ // Force a constant color tint for testing purposes
+
+ image(cubVid, x, y, 320, 240);
+ 
+
+ fill(videoColor);
+  noStroke();
+  rect(x, y, 320, 240);
+
+  
+  // Update the position of the video feed
+  x += xspeed;
+  y += yspeed;
+
+  // Check for canvas borders and change direction if needed
+  if (x > width - 320 || x < 0) {
+    xspeed *= -1;
+    adjustSpeed('x');
+    changeColor();
+    checkCorner();
+  }
+
+  if (y > height - 240 || y < 0) {
+    yspeed *= -1;
+    adjustSpeed('y');
+    changeColor();
+    checkCorner();
+  }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+// Function to change the color of the video feed randomly
+function changeColor() {
+  videoColor = color(random(255), random(255), random(255),100);
+  console.log("Color changed to:", videoColor.toString()); // This will log the color to the console
+ // tint(videoColor); 
 }
+
+// Function to check if the video feed hits one of the corners
+function checkCorner() {
+  if ((x <= 0 && y <= 0) || (x >= width - 320 && y <= 0) || (x <= 0 && y >= height - 240) || (x >= width - 320 && y >= height - 240)) {
+    gong.play(); // Play the ding sound
+  }
+}
+
+function adjustSpeed(axis) {
+    if (speedingUp) {
+      if (axis === 'x') {
+        xspeed *= acceleration;
+        if (Math.abs(xspeed) > maxSpeed) speedingUp = false;
+      } else {
+        yspeed *= acceleration;
+        if (Math.abs(yspeed) > maxSpeed) speedingUp = false;
+      }
+    } else {
+      if (axis === 'x') {
+        xspeed *= deceleration;
+        if (Math.abs(xspeed) < minSpeed) speedingUp = true;
+      } else {
+        yspeed *= deceleration;
+        if (Math.abs(yspeed) < minSpeed) speedingUp = true;
+      }
+    }
+  }
